7d5733c64dec09563f6eb6a8d8314ffb813d6808
Hamza Maach <hamzamaach56@gmail.com>
2025-05-22 16:18:26 +0100

    (matd7kouch) implement 'exit' command

--- src/shell.rs
+++ src/shell.rs
@@ -7,14 +7,55 @@ pub fn run_shell() {
 
         let mut input = String::new();
         if stdin().read_line(&mut input).is_err() {
-            break;
-        }
-
-        // if the command is empty do noting
-        if input.trim().is_empty() {
+            println!("Failed to read input");
             continue;
         }
 
-        println!("0-shell: command not found (yet): {}", input.trim());
+        let trimmed = input.trim();
+
+        if trimmed.is_empty() {
+            continue;
+        }
+
+        // split command and args
+        let mut parts = trimmed.split_whitespace();
+        let command = parts.next().unwrap(); // take the fist part
+
+        match command {
+            "cat" => {
+                println!("'cat' handler is not implemented yet !");
+            }
+            "cd" => {
+                println!("'cd' handler is not implemented yet !");
+            }
+            "cp" => {
+                println!("'cp' handler is not implemented yet !");
+            }
+            "echo" => {
+                println!("'echo' handler is not implemented yet !");
+            }
+            "ls" => {
+                println!("'ls' handler is not implemented yet !");
+            }
+            "mkdir" => {
+                println!("'mkdir' handler is not implemented yet !");
+            }
+            "mv" => {
+                println!("'mv' handler is not implemented yet !");
+            }
+            "pwd" => {
+                println!("'pwd' handler is not implemented yet !");
+            }
+            "rm" => {
+                println!("'rm' handler is not implemented yet !");
+            }
+            "exit" => {
+                println!("Exited seccafully !");
+                break;
+            }
+            _ => {
+                println!("0-shell: command not found : {}", command);
+            }
+        }
     }
 }
