2e1a83ca09f0c37bff11d2f311d227f17e402846
Hamza Maach <hamzamaach56@gmail.com>
2025-05-22 13:25:04 +0100

    continue in case of empty input

--- src/shell.rs
+++ src/shell.rs
@@ -1,12 +1,18 @@
+use std::io::{ stdin, stdout, Write };
+
 pub fn run_shell() {
     loop {
         print!("$ ");
-        use std::io::{ self, Write };
-        io::stdout().flush().unwrap();
+        stdout().flush().unwrap();
 
         let mut input = String::new();
-        if io::stdin().read_line(&mut input).is_err() || input.trim().is_empty() {
+        if stdin().read_line(&mut input).is_err() {
             break;
+        }
+
+        // if the command is empty do noting
+        if input.trim().is_empty() {
+            continue;
         }
 
         println!("0-shell: command not found (yet): {}", input.trim());
