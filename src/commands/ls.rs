ccf78abd801ebb5ec01eb70a07721b9125919ff2
Hamza Maach <hamzamaach56@gmail.com>
2025-05-28 17:57:51 +0100

    handle '-F' flag

--- src/commands/ls.rs
+++ src/commands/ls.rs
@@ -7,31 +7,49 @@ pub struct LsCommand;
 
 impl Command for LsCommand {
     fn execute(&self, args: Vec<String>) -> Result<(), ShellError> {
-        let mut dir = "./";
-        let all_flag = args.contains(&"-a".to_string());
+        let dir = "./";
+        let a_flag = args.contains(&"-a".to_string());
+        let f_flag = args.contains(&"-F".to_string());
 
-        let mut cleaned_paths: Vec<String> = fs::read_dir(dir)
-            .unwrap()
-            .filter_map(Result::ok)
-            .filter_map(|entry| {
-                entry.path().to_str().and_then(|s| {
-                    if s.starts_with("./.") && !all_flag {
-                        // skip hiden files
-                        None
-                    } else {
-                        Some(s.strip_prefix("./").unwrap_or(s).to_string())
+        let mut cleaned_paths: Vec<String> = Vec::new();
+
+        if a_flag { // hard coding hhhhhhh
+            cleaned_paths.push(if f_flag { "./".to_string() } else { ".".to_string() });
+            cleaned_paths.push(if f_flag { "../".to_string() } else { "..".to_string() });
+        }
+
+        cleaned_paths.extend(
+            fs::read_dir(dir)
+                .unwrap()
+                .filter_map(Result::ok)
+                .filter_map(|entry| {
+                    let path = entry.path();
+
+                    if !a_flag {
+                        if let Some(name) = path.file_name().and_then(|n| n.to_str()) {
+                            if name.starts_with('.') {
+                                return None;
+                            }
+                        }
                     }
+
+                    let mut str_name = path.file_name()?.to_str()?.to_string();
+
+                    if f_flag && path.is_dir()  {
+                        str_name.push('/');
+                    }
+
+                    Some(str_name)
                 })
-            })
-            .collect();
+        );
 
         cleaned_paths.sort();
 
-        // Print each cleaned path
         for path in cleaned_paths {
-            print!("{} ", path);
+            print!("{}  ", path);
         }
         println!();
+
         Ok(())
     }
 }
