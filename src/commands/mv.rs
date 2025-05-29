f08d8743c02ffec41cc4d74c33921f0331f15998
yelmach <yassineelmach0@gmail.com>
2025-05-29 12:22:41 +0100

    mv single file

--- src/commands/mv.rs
+++ src/commands/mv.rs
@@ -20,11 +20,28 @@ impl Command for MvCommand {
         }
 
         let (sources, dest) = args.split_at(args.len() - 1);
-
-        let source_path = Path::new(&sources[0]);
         let dest_path = Path::new(&dest[0]);
+        let source_path = Path::new(&sources[0]);
 
-        fs::rename(source_path, dest_path).expect("error in rename func");
-        Ok(())
+        if sources.len() > 1 {
+            move_multiple_files(sources, dest_path)
+        } else {
+            move_single_source(source_path, dest_path)
+        }
     }
+}
+
+fn move_single_source(source: &Path, dest: &Path) -> Result<(), ShellError> {
+    if dest.is_dir() {
+        let dest = dest.join(source.file_name().unwrap());
+        fs::rename(source, &dest)?;
+    } else {
+        fs::rename(source, dest)?;
+    }
+
+    Ok(())
+}
+
+fn move_multiple_files(_sources: &[String], _dest: &Path) -> Result<(), ShellError> {
+    Ok(())
 }
