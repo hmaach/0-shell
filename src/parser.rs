5cca80965d95ec272d7c67b58698d9ecc9023dad
yelmach <yassineelmach0@gmail.com>
2025-05-31 14:28:21 +0100

    implement parser

--- src/parser.rs
+++ src/parser.rs
@@ -1,4 +1,12 @@
 use crate::error::ShellError;
+use std::io::{Write, stdin, stdout};
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+enum ParseState {
+    Normal,
+    SingleQuote,
+    DoubleQuote,
+}
 
 pub fn parse_command(input: String) -> Result<(String, Vec<String>), ShellError> {
     let input = input.trim();
@@ -18,11 +26,109 @@ pub fn parse_command(input: String) -> Result<(String, Vec<String>), ShellError>
     }
 
     let cmd = parts[0].to_string();
-    let args = parts[1..].iter().map(|arg| arg.to_string()).collect();
+    let args = parts[1..].to_vec();
 
     Ok((cmd, args))
 }
 
-fn parser(input: &str) -> Result<Vec<&str>, ShellError> {
-    Ok(input.split_ascii_whitespace().collect())
+fn parser(input: &str) -> Result<Vec<String>, ShellError> {
+    let mut full_input = input.to_string();
+    let mut parts = Vec::new();
+
+    loop {
+        match parse(&full_input) {
+            Ok(parsed_input) => {
+                parts = parsed_input;
+                break;
+            }
+
+            Err(_) => {
+                print!("> ");
+                stdout().flush().unwrap();
+
+                let mut next_line = String::new();
+                match stdin().read_line(&mut next_line) {
+                    Ok(0) => {
+                        return Err(ShellError::Other("Unclosed quote".to_string()));
+                    }
+                    Ok(_) => {
+                        full_input.push('\n');
+                        full_input.push_str(&next_line.trim_end());
+                    }
+                    Err(e) => return Err(ShellError::IoError(e)),
+                }
+            }
+        }
+    }
+
+    Ok(parts)
+}
+
+fn parse(input: &str) -> Result<Vec<String>, ParseState> {
+    let mut result = Vec::new();
+    let mut current_word = String::new();
+    let mut state = ParseState::Normal;
+    let mut chars = input.chars().peekable();
+
+    while let Some(ch) = chars.next() {
+        match state {
+            ParseState::Normal => match ch {
+                ' ' => {
+                    if !current_word.is_empty() {
+                        result.push(current_word.clone());
+                        current_word.clear();
+                    }
+                }
+                '\'' => {
+                    state = ParseState::SingleQuote;
+                }
+                '"' => {
+                    state = ParseState::DoubleQuote;
+                }
+                '\\' => {
+                    if let Some(next_ch) = chars.next() {
+                        current_word.push(next_ch);
+                    }
+                }
+                _ => {
+                    current_word.push(ch);
+                }
+            },
+
+            ParseState::SingleQuote => match ch {
+                '\'' => state = ParseState::Normal,
+                _ => current_word.push(ch),
+            },
+
+            ParseState::DoubleQuote => match ch {
+                '"' => state = ParseState::Normal,
+                '\\' => {
+                    if let Some(next_ch) = chars.next() {
+                        if next_ch == '\\' {
+                            current_word.push(ch);
+                        } else {
+                            current_word.push('\\');
+                            current_word.push(ch);
+                        }
+                    } else {
+                        current_word.push('\\');
+                    }
+                }
+                _ => current_word.push(ch),
+            },
+        }
+    }
+
+    match state {
+        ParseState::DoubleQuote | ParseState::SingleQuote => {
+            return Err(ParseState::DoubleQuote);
+        }
+        ParseState::Normal => {
+            if !current_word.is_empty() {
+                result.push(current_word);
+            }
+        }
+    }
+
+    Ok(result)
 }
