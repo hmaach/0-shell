6b97a36dde893e0fd78455f64fa116224ec80cd0
yelmach <yassineelmach0@gmail.com>
2025-06-05 22:34:26 +0100

    improve cd command

--- src/commands/cd.rs
+++ src/commands/cd.rs
@@ -9,7 +9,20 @@ impl Command for CdCommand {
     fn execute(&self, args: Vec<String>) -> Result<(), ShellError> {
         match args.len() {
             0 => change_to_home(),
-            1 => change_dir(&args[0]),
+            1 => match args[0].as_str() {
+                "-" => change_to_previous(),
+                "~" => change_to_home(),
+                path if path.starts_with("~/") => match env::var("HOME") {
+                    Ok(home_dir) => {
+                        let expanded_path = path.replace("~", &home_dir);
+                        change_dir(&expanded_path)
+                    }
+                    Err(_) => Err(ShellError::Other(
+                        "cd: HOME environment variable not set".to_string(),
+                    )),
+                },
+                path => change_dir(path),
+            },
             _ => Err(ShellError::Other("cd: too many arguments".to_string())),
         }
     }
@@ -24,7 +37,30 @@ fn change_to_home() -> Result<(), ShellError> {
     }
 }
 
+fn change_to_previous() -> Result<(), ShellError> {
+    match env::var("OLDPWD") {
+        Ok(old_dir) => {
+            let current_dir = env::current_dir().map_err(|e| {
+                ShellError::Other(format!("cd: cannot get current directory: {}", e))
+            })?;
+
+            change_dir(&old_dir)?;
+
+            unsafe { env::set_var("OLDPWD", current_dir) };
+
+            println!("{}", old_dir);
+
+            Ok(())
+        }
+        Err(_) => Err(ShellError::Other("cd: OLDPWD not set".to_string())),
+    }
+}
+
 fn change_dir(path: &str) -> Result<(), ShellError> {
+    if let Ok(current_dir) = env::current_dir() {
+        unsafe { env::set_var("OLDPWD", current_dir) };
+    }
+
     let target_path = Path::new(path);
 
     if !target_path.exists() {
