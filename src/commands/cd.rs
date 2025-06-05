5292b7e72c4c58aa9c97b636e6c65657e90cc982
yelmach <yassineelmach0@gmail.com>
2025-06-05 22:33:59 +0100

    accept 0 arg in cd command

--- src/commands/cd.rs
+++ src/commands/cd.rs
@@ -7,9 +7,40 @@ pub struct CdCommand;
 
 impl Command for CdCommand {
     fn execute(&self, args: Vec<String>) -> Result<(), ShellError> {
-        if let Err(err) = env::set_current_dir(Path::new(&args[0])) {
-            return Err(ShellError::Other(err.to_string()));
+        match args.len() {
+            0 => change_to_home(),
+            1 => change_dir(&args[0]),
+            _ => Err(ShellError::Other("cd: too many arguments".to_string())),
         }
-        Ok(())
     }
+}
+
+fn change_to_home() -> Result<(), ShellError> {
+    match env::var("HOME") {
+        Ok(home_dir) => change_dir(&home_dir),
+        Err(_) => Err(ShellError::Other(
+            "cd: HOME environment variable not set".to_string(),
+        )),
+    }
+}
+
+fn change_dir(path: &str) -> Result<(), ShellError> {
+    let target_path = Path::new(path);
+
+    if !target_path.exists() {
+        return Err(ShellError::Other(format!(
+            "cd: {}: No such file or directory",
+            path
+        )));
+    }
+
+    if !target_path.is_dir() {
+        return Err(ShellError::Other(format!("cd: {}: Not a directory", path)));
+    }
+
+    if let Err(err) = env::set_current_dir(target_path) {
+        return Err(ShellError::Other(err.to_string()));
+    }
+
+    Ok(())
 }
