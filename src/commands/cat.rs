15776bcfc66582b7da23edc4db2025d4369192f2
Hamza Maach <hamzamaach56@gmail.com>
2025-06-13 09:23:55 +0100

    cat command when no args given

--- src/commands/cat.rs
+++ src/commands/cat.rs
@@ -1,10 +1,24 @@
+use std::io::stdin;
+
 use crate::commands::Command;
 use crate::error::*;
 
 pub struct CatCommand;
 
 impl Command for CatCommand {
-    fn execute(&self, _args: Vec<String>) -> Result<(), ShellError> {
+    fn execute(&self, args: Vec<String>) -> Result<(), ShellError> {
+        if args.is_empty() {
+            loop {
+                let mut input = String::new();
+                if let Err(e) = stdin().read_line(&mut input) {
+                    eprintln!("cat: error reading the input: {}", e);
+                    continue;
+                }
+                
+                print!("{}", &input);
+            }
+        } else {
+        }
         Ok(())
     }
 }
